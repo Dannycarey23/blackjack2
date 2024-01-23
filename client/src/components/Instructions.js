ccc41514fac18d32367db092900dc8cdbb4c9798
Danny <carey.d@ntlworld.com>
2024-01-23 16:37:58 +0000

    added a small amount of  css to the instructions popup

--- client/src/components/Instructions.js
+++ client/src/components/Instructions.js
@@ -9,8 +9,8 @@ const Instructions = () => {
 
     return(
         <Popup trigger={<button className="bjBtns"> Rules</button>} position="bottom left" >
-          <div>
-            <h1> "House" Rules:</h1>
+          <div className="rulesDiv">
+            <h1><u>House Rules</u></h1>
             <ul>
                 <li>Aim to have the value of all your cards add up to 21 without going over </li>
                 <li>You win if your total is closer to 21 than the dealers is </li>
